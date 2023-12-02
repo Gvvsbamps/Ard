fa1c40568ffae6de1a6d1375514ed412e885f586
gvvsbamps <gvvsbamps@gmail.com>
2023-12-02 14:50:35 +0800

    f

--- src/Controller/CurrencyRateController.php
+++ src/Controller/CurrencyRateController.php
@@ -13,29 +13,44 @@ class CurrencyRateController extends AbstractController
     #[Route('/currency/rate', name: 'app_currency_rate')]
     public function index(Request $request): JsonResponse
     {
-        // Replace 'YOUR_ACCESS_KEY' with your actual API access key
-        $accessKey = 'n6BvkdPjAvX772cYZ0zrBwRWbsJn9p';
-        $baseUrl = 'https://j2me.mostmoney.mn:9097/api/fi/v1.0/getScMarketInfo';
+        $apiUrl = 'https://j2me.mostmoney.mn:9097/api/fi/v1.0/getScMarketInfo';
+        $accessToken = 'n6BvkdPjAvX772cYZ0zrBwRWbsJn9p';
 
-        // Define parameters for the API request
-        $params = [
-            'nessession' => $accessKey,
+        $requestData = [
+            'brokerId' => '32',
+            'securityCode' => 'AARD-O-0000',
+            'infoType' => '',
+            'mostId' => '',
+            'connId' => 0,
+            'marketCode' => '',
+            'affCustId' => '',
+            'srcFiCode' => '32',
+            'traceNo' => '',
+            'wallet' => '',
         ];
 
-        // Build the URL with parameters
-        $url = $baseUrl . '?' . http_build_query($params, '', '&', PHP_QUERY_RFC3986);
-
-        // Make API request using Symfony HttpClient
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', $url);
 
-        // Get the response content as an array
-        $data = $response->toArray();
-
-        // Your controller logic here
-        return $this->json([
-            'message' => 'Exchange rates retrieved successfully!',
-            'data' => $data,
+        $response = $httpClient->request('POST', $apiUrl, [
+            'headers' => [
+                'nessession' => $accessToken,
+                'Content-Type' => 'application/json',
+            ],
+            'json' => $requestData,
         ]);
+
+        if ($response->getStatusCode() >= 200 && $response->getStatusCode() < 300) {
+            $data = $response->toArray();
+
+            return $this->json([
+                'message' => 'Exchange rates retrieved successfully!',
+                'data' => $data,
+            ]);
+        } else {
+            return $this->json([
+                'error' => 'Failed to retrieve exchange rates.',
+                'statusCode' => $response->getStatusCode(),
+            ], $response->getStatusCode());
+        }
     }
 }

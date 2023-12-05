9460878981b4d7d6c7f4a064de4ec0e2faa3282c
gvvsbamps <gvvsbamps@gmail.com>
2023-12-05 18:00:15 +0800

    f

--- src/Controller/CurrencyRateController.php
+++ src/Controller/CurrencyRateController.php
@@ -15,27 +15,32 @@ class CurrencyRateController extends AbstractController
     {
         // Replace 'YOUR_ACCESS_KEY' with your actual API access key
         $accessKey = 'n6BvkdPjAvX772cYZ0zrBwRWbsJn9p';
-        $baseUrl = 'https://j2me.mostmoney.mn:9097/api/fi/v1.0/getScMarketInfo';
+        $apiUrl = 'https://j2me.mostmoney.mn:9097/api/fi/v1.0/getScMarketInfo';
 
-        // Define parameters for the API request
-        $params = [
-            'nessession' => $accessKey,
-        ];
-
-        // Build the URL with parameters
-        $url = $baseUrl . '?' . http_build_query($params, '', '&', PHP_QUERY_RFC3986);
-
-        // Make API request using Symfony HttpClient
+        // Make API request using Symfony HttpClient with access key in the header
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', $url);
-
-        // Get the response content as an array
-        // $data = $response->toArray();
-
-        // Your controller logic here
-        return $this->json([
-            'message' => 'Exchange rates retrieved successfully!',
-            'data' => $response,
+        $response = $httpClient->request('GET', $apiUrl, [
+            'headers' => [
+                'nessession' => $accessKey,
+                'Content-Type' => 'application/json',
+            ],
         ]);
+
+        // Check the status code and handle the response accordingly
+        $statusCode = $response->getStatusCode();
+        if ($statusCode >= 200 && $statusCode < 300) {
+            // Get the response content as an array
+            // $data = $response->toArray();
+
+            return $this->json([
+                'message' => 'Exchange rates retrieved successfully!',
+                'data' => $response,
+            ]);
+        } else {
+            return $this->json([
+                'error' => 'Failed to retrieve exchange rates.',
+                'statusCode' => $statusCode,
+            ], $statusCode);
+        }
     }
 }

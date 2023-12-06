487f9b242e492059cf42402217432a869f627995
gvvsbamps <gvvsbamps@gmail.com>
2023-12-06 17:06:30 +0800

    gix

--- src/Controller/CurrencyRateController.php
+++ src/Controller/CurrencyRateController.php
@@ -13,32 +13,44 @@ class CurrencyRateController extends AbstractController
     #[Route('/', name: 'app_currency_rate')]
     public function index(Request $request): JsonResponse
     {
-        // Replace 'YOUR_ACCESS_KEY' with your actual API access key
         $accessKey = 'n6BvkdPjAvX772cYZ0zrBwRWbsJn9p';
         $apiUrl = 'https://j2me.mostmoney.mn:9097/api/fi/v1.0/getScMarketInfo';
 
-        // Make API request using Symfony HttpClient with access key in the header
+        $requestData = [
+            'brokerId' => '32',
+            'securityCode' => 'AARD-O-0000',
+            'infoType' => '',
+            'mostId' => '',
+            'connId' => 0,
+            'marketCode' => '',
+            'affCustId' => '',
+            'srcFiCode' => '32',
+            'traceNo' => '',
+            'wallet' => '',
+        ];
+
         $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', $apiUrl, [
+        $response = $httpClient->request('POST', $apiUrl, [
             'headers' => [
                 'nessession' => $accessKey,
                 'Content-Type' => 'application/json',
             ],
+            'json' => $requestData,
         ]);
 
-        // Check the status code and handle the response accordingly
         $statusCode = $response->getStatusCode();
         if ($statusCode >= 200 && $statusCode < 300) {
             // Get the response content as an array
             // $data = $response->toArray();
 
             return $this->json([
-                'message' => 'Exchange rates retrieved successfully!',
+                'message' => 'Data retrieved successfully!',
                 'data' => $response,
             ]);
         } else {
             return $this->json([
-                'error' => 'Failed to retrieve exchange rates.',
+                'error' => 'Failed to retrieve data.',
+                'data' => $response->getContent(),
                 'statusCode' => $statusCode,
             ], $statusCode);
         }

59eb26720b2a9ef95870be902695f37b8c12c867
gvvsbamps <gvvsbamps@gmail.com>
2024-01-09 14:32:48 +0800

    mailer

--- src/Controller/CurrencyRateController.php
+++ src/Controller/CurrencyRateController.php
@@ -7,10 +7,19 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mailer\Exception\TransportException;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Response;
+
+
 
 class CurrencyRateController extends AbstractController
 {
-    #[Route('/', name: 'app_currency_rate',)]
+    /**
+     * @Route("/", name="app_index")
+     */
     public function index(Request $request): JsonResponse
     {
         $accessKey = 'n6BvkdPjAvX772cYZ0zrBwRWbsJn9p';
@@ -54,5 +63,32 @@ class CurrencyRateController extends AbstractController
                 'statusCode' => $statusCode,
             ], $statusCode);
         }
+    }
+    /**
+     * @Route("/proseed-mailer", name="app_mailer",method={"GET"})
+     */
+    public function mailer(Request $request, MailerInterface $mailer): Response
+    {
+        $name = $request->get('name');
+        $email = $request->get('email');
+        $email2 = $request->get('email-2');
+        $field = $request->get('field');
+
+        $email = (new Email())
+            ->from('proseedagency@gmail.com')
+            ->to('proseedagency@gmail.com')
+            ->subject('Үйлчлүүлэгч цаг авлаа.')
+            ->text('Нэр: ' . $name . ' Утас: ' . $email . ' И-мейл: ' . $email2 . ' Чиглэл: ' . $field);
+        $emailUser = (new Email())
+            ->from('proseedagency@gmail.com')
+            ->to($email2)
+            ->subject('Proseed Agency')
+            ->text('Амжилттай илгээгдлээ. Бид танд тохирох туршлагатай мэргэжилтнийг санал болгох болно.' . "\n" . 'Баярлалаа
+            ');
+
+        $mailer->send($email);
+        $mailer->send($emailUser);
+
+        return new Response('Success');
     }
 }

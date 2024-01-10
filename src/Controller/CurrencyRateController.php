487a5ba04a99e916e541809b22afd88a830e0b44
topuk <orgil.num@gmail.com>
2024-01-10 14:24:45 +0800

    update request

--- src/Controller/CurrencyRateController.php
+++ src/Controller/CurrencyRateController.php
@@ -63,34 +63,4 @@ class CurrencyRateController extends AbstractController
         }
     }
 
-    /**
-     * @Route("/proseed-mailer", name="app_mailer", methods={"GET"})
-     */
-    public function mailer(Request $request, MailerInterface $mailer): Response
-    {
-        $name = $request->get('name');
-        $email = $request->get('email');
-        $email2 = $request->get('email-2');
-        $field = $request->get('field');
-
-        $email = (new Email())
-            ->from('proseedagency@gmail.com')
-            ->to('proseedagency@gmail.com')
-            ->subject('Үйлчлүүлэгч цаг авлаа.')
-            ->text('Нэр: ' . $name . ' Утас: ' . $email . ' И-мейл: ' . $email2 . ' Чиглэл: ' . $field);
-        $emailUser = (new Email())
-            ->from('proseedagency@gmail.com')
-            ->to($email2)
-            ->subject('Proseed Agency')
-            ->text('Амжилттай илгээгдлээ. Бид танд тохирох туршлагатай мэргэжилтнийг санал болгох болно.' . "\n" . 'Баярлалаа
-            ');
-        try {
-            $mailer->send($email);
-            $mailer->send($emailUser);
-        }catch (\ErrorException $e){
-            return new Response('Error');
-        }
-
-        return new Response('Success');
-    }
 }

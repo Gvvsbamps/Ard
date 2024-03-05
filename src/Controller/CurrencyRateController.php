19435e9c3b4545c8125a16cd00eb7f7fc5816c95
Gvvsbamps <gvvsbamps@gmail.com>
2024-03-05 19:02:27 +0800

    d

--- src/Controller/CurrencyRateController.php
+++ src/Controller/CurrencyRateController.php
@@ -2,57 +2,108 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpClient\HttpClient;
+use GuzzleHttp\Client;
+
+set_time_limit(300);
 
 class CurrencyRateController extends AbstractController
 {
-    #[Route('/', name: 'app_currency_rate',)]
-    public function index(Request $request): JsonResponse
-    {
-        $accessKey = 'n6BvkdPjAvX772cYZ0zrBwRWbsJn9p';
-        // $apiUrl = 'https://j2me.mostmoney.mn:9097/api/fi/v1.0/getScMarketInfo';
-        $apiUrl = 'https://j2me.mostmoney.mn:9097/api/fi/v1.0/getSecurityList';
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index(
+        EntityManagerInterface $entityManager,
+    ): Response {
+        $conn = $entityManager->getConnection();
+        $sql = "SELECT namemn,nameen,companycode,logo,symbol,volume,zarlasan,segments,state_own,state_own_date,adjustmentcoef FROM companyinfo where logo is not null";
+        $stmt = $conn->prepare($sql);
+        $result = $stmt->executeQuery();
+        $data = $result->fetchAllAssociative();
 
-        $requestData = [
-            'connId' => 0,
-            'brokerId' => '',
-            'mostId' => '',
-            'infoType' => 'A',
-            'type' => 'SEC',
-            'marketCode' => 'MSE',
-            'wallet' => '',
-            'affCustId' => '',
-            'srcFiCode' => '',
-            'traceNo' => '',
-        ];
 
-        $httpClient = HttpClient::create();
-        $response = $httpClient->request('POST', $apiUrl, [
-            'headers' => [
-                'nessession' => $accessKey,
-                'Content-Type' => 'application/json',
-            ],
-            'json' => $requestData,
+        // $httpClient = HttpClient::create();
+        $items = array();
+        // $statuses = array();
+        $client = new Client();
+
+        // foreach ($data as $record) {
+        //     $client->request('POST', 'https://api.webflow.com/v2/collections/65e560e5ee90ddba8e6cd7ab/items/live', [
+        //         'json' => [
+        //             'isArchived' => false,
+        //             'isDraft' => false,
+        //             'fieldData' => [
+        //                 'name' => (string)$record['namemn'] ?? '',
+        //                 'slug' => $record['symbol'],
+        //                 'namemn' => (string)$record['namemn'] ?? '',
+        //                 'nameen' => (string)$record['nameen'] ?? '',
+        //                 'logo' => (string)$record['logo'] ?? '',
+        //                 'zarlasan' => (string)$record['zarlasan'] ?? '',
+        //                 'volume' => (string)$record['volume'] ?? '',
+        //                 'symbol' => isset($record['symbol']) ? (string)$record['symbol'] : '',
+        //                 'adjustmentcoef' => isset($record['adjustmentcoef']) ? (string)$record['adjustmentcoef'] : '',
+        //                 'companycode' => isset($record['companycode']) ? (string)$record['companycode'] : ''
+        //             ]
+        //         ],
+        //         'headers' => [
+        //             'accept' => 'application/json',
+        //             'authorization' => 'Bearer d26b568087561b2f83e1d2e3bcd5409c2300f745b8db3facb7662f137b27e22a',
+        //             'content-type' => 'application/json',
+        //         ],
+        //     ]);
+        //     array_push($items, $record['companycode']);
+        //     // array_push($statuses, $response->getStatusCode());
+        // }
+        return $this->json([
+            'status' => $items,
         ]);
+    }
+    /**
+     * @Route("/detail", name="app_detail")
+     */
+    public function detail(
+        EntityManagerInterface $entityManager,
+    ): Response {
+        $conn = $entityManager->getConnection();
+        $sql = "SELECT namemn,nameen,companycode,logo,symbol,volume,zarlasan,segments,state_own,state_own_date,adjustmentcoef FROM companyinfo where logo is not null";
+        $stmt = $conn->prepare($sql);
+        $result = $stmt->executeQuery();
+        $data = $result->fetchAllAssociative();
 
-        $statusCode = $response->getStatusCode();
-        if ($statusCode >= 200 && $statusCode < 300) {
-            // Get the response content as an array
-            $data = $response->toArray();
-
-            return $this->json([
-                'message' => 'Data retrieved successfully!',
-                'data' => $data,
-            ]);
-        } else {
-            return $this->json([
-                'error' => 'Failed to retrieve data.',
-                'statusCode' => $statusCode,
-            ], $statusCode);
-        }
+        // foreach ($data as $record) {
+        //     $client->request('POST', 'https://api.webflow.com/v2/collections/65e560e5ee90ddba8e6cd7ab/items/live', [
+        //         'json' => [
+        //             'isArchived' => false,
+        //             'isDraft' => false,
+        //             'fieldData' => [
+        //                 'name' => (string)$record['namemn'] ?? '',
+        //                 'slug' => $record['symbol'],
+        //                 'namemn' => (string)$record['namemn'] ?? '',
+        //                 'nameen' => (string)$record['nameen'] ?? '',
+        //                 'logo' => (string)$record['logo'] ?? '',
+        //                 'zarlasan' => (string)$record['zarlasan'] ?? '',
+        //                 'volume' => (string)$record['volume'] ?? '',
+        //                 'symbol' => isset($record['symbol']) ? (string)$record['symbol'] : '',
+        //                 'adjustmentcoef' => isset($record['adjustmentcoef']) ? (string)$record['adjustmentcoef'] : '',
+        //                 'companycode' => isset($record['companycode']) ? (string)$record['companycode'] : ''
+        //             ]
+        //         ],
+        //         'headers' => [
+        //             'accept' => 'application/json',
+        //             'authorization' => 'Bearer d26b568087561b2f83e1d2e3bcd5409c2300f745b8db3facb7662f137b27e22a',
+        //             'content-type' => 'application/json',
+        //         ],
+        //     ]);
+        //     array_push($items, $record['companycode']);
+        //     // array_push($statuses, $response->getStatusCode());
+        // }
+        return $this->json([
+            'status' => $data,
+        ]);
     }
 }
